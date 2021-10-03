deaba9b450d09a49d0540b3b91b95fa0028d3789
KimJihun13157ff <91823003+KimJihun13157ff@users.noreply.github.com>
2021-10-03 19:17:08 +0900

    Add files via upload

---
+++ week1/prob1.js
@@ -0,0 +1,9 @@
+let k;
+for(let i=1; i <=9; i++)
+{
+    for(let j=1; j <=9; j++)
+    {
+        k = i * j;
+        console.log(i + " x " + j + " = " + k);
+    }
+}
---
+++ week1/prob3.js
@@ -0,0 +1,14 @@
+let myProfile =
+{
+    "name" : "Kim Jihun",
+    "job" : "student",
+    "age" : 19,
+    "hobby" : ["LOL", "Badminton", ""],
+    "course_table":[
+        {"name" : "데이터구조", "id" : "CSED233"},
+        {"name" : "오토마타및형식언어", "id" : "CSED341"},
+        {"name" : "확률및통계", "id" : "MATH230"},
+        {"name" : "컴퓨터 SW시스템 개론", "id" : "CSED211"},
+        {"name" : "음악의 이해", "id" : "ARTS313"}
+    ]
+}
